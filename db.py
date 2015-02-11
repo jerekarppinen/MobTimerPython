1a55bda60ce98f385a94d72442ba54c541b9e6f5
jerekarppinen <jere.karppinen88@gmail.com>
2015-02-11 20:39:58 +0200

    initial db file

---
+++ db.py
@@ -0,0 +1,36 @@
+import sqlite3
+
+try:
+	db = sqlite3.connect("usersDB")
+	cursor = db.cursor()
+	cursor.execute(''' CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, name TEXT)''')
+	db.commit()
+
+except Exception as e:
+	# Rollback any changes if something went wrong
+	db.rollback()
+	raise e
+
+finally:
+	db.close()
+
+#cursor.execute('''CREATE TABLE users(id INTEGER PRIMARY KEY, name TEXT)''')
+#db.commit()
+
+#cursor.execute(''' DROP TABLE users ''')
+#db.commit()
+
+#cursor.execute(''' SELECT name FROM sqlite_master WHERE type='table' AND name='users' ''')
+
+#if cursor.fetchone() is None:
+#		print "is empty"
+#else:
+#	print "not empty"
+#	print len(cursor.fetchall())
+
+#print db.commit()
+
+
+#cursor.execute(''' INSERT INTO users ())
+
+#db.close()
